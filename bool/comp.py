6d543b7ddb41fd26f04ec8ad8f95121b6d05e374
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-26 21:25:55 +0700

    init

---
+++ bool/comp.py
@@ -0,0 +1,5 @@
+print('cat' > 'Cat')  # huruf kecil lebih besar
+
+print(not 42 == 'Answer')
+
+print(25 > 50 or 1 != 2)
---
+++ function/return.py
@@ -0,0 +1,5 @@
+
+def area_triangle(base, height):
+    return base * height / 2
+
+print('The area is: ' + str(area_triangle(4, 4)))
