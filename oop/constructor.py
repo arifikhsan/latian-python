6a8f3d42bb44daed66187758c747dec9313bbf9c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-28 21:34:23 +0700

    str

--- oop/constructor.py
+++ oop/constructor.py
@@ -3,6 +3,11 @@ class Apple:
         self.color = color
         self.flavor = flavor
 
+    def __str__(self):
+        return 'color: {}, flavor: {}'.format(self.color, self.flavor)
+
 apple = Apple('red', 'sweet')
 print(apple.color)
 print(apple.flavor)
+
+print(str(apple))
