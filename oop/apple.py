45b78388a7e5c64b642068cfa628f38240d7a410
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-28 21:32:32 +0700

    apple

---
+++ oop/apple.py
@@ -0,0 +1,18 @@
+class Apple:
+    color = ''
+    flavor = ''
+
+apple = Apple()
+apple.color = 'red'
+apple.flavor = 'sweet'
+
+print(apple.color)
+print(apple.flavor)
+print(apple.color.upper())
+
+golden = Apple()
+golden.color = 'Yellow'
+golden.flavor = 'Soft'
+
+print(golden.color)
+print(golden.flavor)
