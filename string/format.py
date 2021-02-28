72ffbf74e989bc86ee7c99a7a786ba12a09b8ec6
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-28 20:30:04 +0700

    format

---
+++ string/format.py
@@ -0,0 +1,7 @@
+print('name: {}'.format('arif'))
+name = 'arif'
+print(f'hello, {name}')
+print('price: ${:.2f}'.format(3.222))
+
+print('{:>3} F | {:>6.2f} C'.format(10, 12.222))
+print('{:>3} F | {:>6.2f} C'.format(10, 1.45))
