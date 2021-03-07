4f6245646da5db3bee55597fd0864cc771a251ab
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-07 21:53:12 +0700

    datetime

--- os/files/os_operation.py
+++ os/files/os_operation.py
@@ -1,6 +1,15 @@
 import os
+import datetime
 
 os.remove('novel.txt')
 os.rename('before.txt', 'after.txt')
 
 os.path.exists('finished_masterpiece.txt')
+os.path.getsize('spider.txt')
+os.path.getmtime('spider.txt')
+
+timestamp = os.path.getmtime('spider.txt')
+
+datetime.datetime.fromtimestamp(timestamp)
+
+os.path.abspath('spider.txt')
