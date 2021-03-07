f966441e4773f4c2d9ff4da482189ec9a2b02d74
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-07 22:39:34 +0700

    directory

---
+++ os/directory/dir.py
@@ -0,0 +1,10 @@
+import os
+
+print(os.getcwd())
+
+os.mkdir('new_dir')
+os.chdir('new_dir')
+
+os.getcwd()
+
+os.listdir('website')
---
+++ os/directory/parent.py
@@ -0,0 +1,11 @@
+import os
+def parent_directory():
+  # Create a relative path to the parent
+  # of the current working directory
+  relative_parent = os.path.join('.', os.pardir)
+
+  # Return the absolute path of the parent directory
+  return os.path.abspath(relative_parent)
+
+print(os.getcwd())
+print(parent_directory())
