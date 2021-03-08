548fe1a95cf178496c8a8326613b454310c74c2e
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-08 22:09:04 +0700

    grep sh

---
+++ os/regex/grep.sh
@@ -0,0 +1,14 @@
+# search grep
+grep thon /usr/share/dict/words
+
+# search ignore case
+grep -i python /usr/share/dict/words
+
+# dot for replace of any other character
+grep l.rts /usr/share/dict/words
+
+# start with fruit
+grep ^fruit /usr/share/dict/words
+
+# end with cat
+grep cat$ /usr/share/dict/words
