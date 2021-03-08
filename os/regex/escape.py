4f3b2220ed7f2df3ede216bbc7cb5f3dfd95bbef
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-08 22:53:51 +0700

    escape character

---
+++ os/regex/escape.py
@@ -0,0 +1,9 @@
+import re
+
+# escape
+print(re.search(r'.com', 'welcome'))
+print(re.search(r'\.com', 'welcome'))
+print(re.search(r'\.com', 'mydomain.com'))
+
+print(re.search(r'\w*', 'This is an example'))
+print(re.search(r'\w*', 'And_this_is_another'))
