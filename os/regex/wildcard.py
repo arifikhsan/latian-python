42792fd35c9442be260d74efb8e1dd60ecd77cc4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-08 22:29:54 +0700

    wildcard

---
+++ os/regex/wildcard.py
@@ -0,0 +1,7 @@
+import re
+
+print(re.search(r'[Pp]ython', 'Python'))
+print(re.search(r'[a-z]way', 'The end of the highway'))
+print(re.search(r'[a-z]way', 'What a way to go'))
+print(re.search(r'cloud[a-zA-Z0-9]', 'cloudy'))
+print(re.search(r'cloud[a-zA-Z0-9]', 'cloud9'))
