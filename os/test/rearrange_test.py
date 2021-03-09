dcb9d651580aca95cd9a0b07356b2b5bcc388229
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-09 23:04:54 +0700

    unittest

--- os/test/rearrange_test.py
+++ os/test/rearrange_test.py
@@ -1,3 +1,10 @@
 from rearrange import rearrange_name
+import unittest
 
-print(rearrange_name('Lovelace, Ada'))
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = 'Lovelace, Ada'
+        expected = 'Ada Lovelace'
+        self.assertEqual(rearrange_name(testcase), expected)
+
+unittest.main()
