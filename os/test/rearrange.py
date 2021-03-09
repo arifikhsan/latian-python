c21a6c2925433670f96fe144bb587b57aba72e23
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-09 23:11:17 +0700

    test_empty

--- os/test/rearrange.py
+++ os/test/rearrange.py
@@ -3,9 +3,8 @@ import re
 def rearrange_name(name):
     result = re.search(r'^([\w \.-]*), ([\w \.-]*)$', name)
     if result is None:
-        return name
-    else:
-        return '{} {}'.format(result[2], result[1])
+        return ''
+    return '{} {}'.format(result[2], result[1])
 
 # print(rearrange_name('Ikhsanudin, Arif'))
 # print(rearrange_name('Lovelace, Ada'))
