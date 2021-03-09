11ca783ef83daa98bb4929685312b9ee85f1f6ea
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-03-09 23:15:49 +0700

    test_double_name

--- os/test/rearrange_test.py
+++ os/test/rearrange_test.py
@@ -12,4 +12,14 @@ class TestRearrange(unittest.TestCase):
         expected = ''
         self.assertEqual(rearrange_name(testcase), expected)
 
+    def test_double_name(self):
+        testcase = 'Hopper, Grace M.'
+        expected = 'Grace M. Hopper'
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = 'Voltaire'
+        expected = 'Voltaire'
+        self.assertEqual(rearrange_name(testcase), expected)
+
 unittest.main()
